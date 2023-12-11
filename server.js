c1176a6dfaebd08b9e53c1ba0cfd7a7b02c7366e
Eduarda <eduardabpetry@gmail.com>
2023-12-11 10:23:03 +0100

    Try out path methods

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 console.log("Hello Wold");
 console.log("This is a console log from my server.js file");
+console.log(__dirname);
+console.log(__filename);
+
+const path = require("path");
+// Base file name
+console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__filename));
+
+// File extension
+console.log(path.extname(__filename));
+
+// Create path object
+console.log(path.parse(__filename));
+
+// Get property from path object
+console.log(path.parse(__filename).base);
+
+// Concatenate paths
+console.log(path.join(__dirname, "test", "hello.html"));
