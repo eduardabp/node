38ac37c0f0aab5710267b9c727507d2b90622941
Eduarda <eduardabpetry@gmail.com>
2023-12-11 12:02:51 +0100

    Create main page of client directory

--- server.js
+++ server.js
@@ -1,72 +1,34 @@
-console.log("Hello Wold");
-console.log("This is a console log from my server.js file");
-console.log(__dirname);
-console.log(__filename);
-
 const path = require("path");
-// Base file name
-console.log(path.basename(__filename));
-
-// Directory name
-console.log(path.dirname(__filename));
-
-// File extension
-console.log(path.extname(__filename));
-
-// Create path object
-console.log(path.parse(__filename));
-
-// Get property from path object
-console.log(path.parse(__filename).base);
-
-// Concatenate paths
-console.log(path.join(__dirname, "test", "hello.html"));
 
 const fs = require("fs");
 
 // Create folder
-// Creating a folder
+
 try {
-    fs.mkdirSync(path.join(__dirname, "test"));
-  
-    // fs.writeFileSync(path.join(__dirname, "test", "log.txt"), "Hello World")
+    fs.mkdirSync(path.join(__dirname, "client"));
   } catch (err) {
     console.error("Error code :", err.code);
     console.error(err.message);
   }
   
-  // Creating and writing in a file
+  // Creating and writing index.html and style.css
   try {
-    const filePath = path.join(__dirname, "test", "log.txt");
+    const filePath = path.join(__dirname, "client", "index.html");
   
-    fs.writeFileSync(filePath, "Hello World!");
+    fs.writeFileSync(filePath, "<head><link href='style.css' rel='stylesheet'></head>");
   
-    fs.appendFileSync(filePath, " I love Node JS");
+    fs.appendFileSync(filePath, "<body><h1>This is my main page</h1></body>");
   } catch (err) {
     console.error(err.code);
     console.error(err.message);
   }
-  
-  // Read file
+
   try {
-    const fileContent = fs.readFileSync(
-      path.join(__dirname, "test", "log.txt"),
-      "utf-8"
-    );
+    const filePath = path.join(__dirname, "client", "style.css");
   
-    console.log(fileContent);
+    fs.writeFileSync(filePath, "body {background-color: blue}");
   } catch (err) {
     console.error(err.code);
     console.error(err.message);
   }
-  
-  // Rename file
-  try {
-    fs.renameSync(
-      path.join(__dirname, "test", "log.txt"),
-      path.join(__dirname, "test", "new-log.txt")
-    );
-  } catch (err) {
-    console.error(err.code);
-    console.error(err.message);
-  }
+
