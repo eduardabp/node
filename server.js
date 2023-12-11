57dbfeb85dadaad386cb67387d8e219941d5277b
Eduarda <eduardabpetry@gmail.com>
2023-12-11 12:09:25 +0100

    Add script to create contact page

--- server.js
+++ server.js
@@ -32,3 +32,33 @@ try {
     console.error(err.message);
   }
 
+  // Create contact folder
+
+  try {
+    fs.mkdirSync(path.join(__dirname, "client", "contact"));
+  } catch (err) {
+    console.error("Error code :", err.code);
+    console.error(err.message);
+  }
+  
+  // Creating and writing index.html and style.css
+  try {
+    const filePath = path.join(__dirname, "client", "contact", "index.html");
+  
+    fs.writeFileSync(filePath, "<head><link href='style.css' rel='stylesheet'></head>");
+  
+    fs.appendFileSync(filePath, "<body><h1>This is the contact page</h1></body>");
+  } catch (err) {
+    console.error(err.code);
+    console.error(err.message);
+  }
+
+  try {
+    const filePath = path.join(__dirname, "client", "contact", "style.css");
+  
+    fs.writeFileSync(filePath, "body {background-color: yellow}");
+  } catch (err) {
+    console.error(err.code);
+    console.error(err.message);
+  }
+
